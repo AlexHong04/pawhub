514ea0c72932ad23615ff71cf912b347dc064ba3
AlexHong04 <145246775+AlexHong04@users.noreply.github.com>
2026-03-04 16:00:40 +0800

    fix version

--- lib/core/supabase/supabase_config.dart
+++ lib/core/supabase/supabase_config.dart
@@ -1,4 +1,4 @@
 class supabaseConfig {
-  static const url = '';
-  static const key = '';
+  static const url = 'https://yhzbvypawqxkmnprsfnu.supabase.co';
+  static const key = 'sb_secret_drYQzq2gGPaToq39t5XgkA_bO8wJ2Kc';
 }
